4c255b0b895f6230f6d97944381db9437c94db90
Bhavya-is-great <bdopcreator123@gmail.com>
2025-09-29 21:38:35 +0530

    Some basic setup

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,16 +1,18 @@
 // app/page.jsx
 import React from "react";
+import GlowBUtton from "@/ui/GlowBUtton";
 
 export default function Home() {
   return (
     <main className="landing-page">
       <div className="landing-content">
-        <h1>Hello, I&apos;m Bhavya</h1>
+        {/* <h1>Hello, I&apos;m Bhavya</h1>
         <h2>Full Stack Developer | React | Node.js | PHP</h2>
         <div className="btn-group">
           <button className="btn">View Projects</button>
           <button className="btn">Contact Me</button>
-        </div>
+        </div> */}
+          <GlowBUtton> Hello this is button </GlowBUtton>
       </div>
     </main>
   );
